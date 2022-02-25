7cb4e920724a6e88055d36afc21b9b6589b9ae85
andradeviniicius <viniciusdandrade01@gmail.com>
2022-02-25 16:02:34 -0300

    refatorando código

--- simplePort/scripts/main.js
+++ simplePort/scripts/main.js
@@ -7,27 +7,31 @@ toggleLanguage.addEventListener('click',()=>{
             const h2 = document.querySelector('header h2')
             const currBulma = document.querySelector('#currBulma')
             const liveChat = document.querySelector('#liveChat')
+            const socialMedia = document.querySelector('.userMedia p')
             h1.innerHTML='Olá, seja bem vindo ao meu portifólio!'
             h2.innerHTML='Aqui você consegue acessar todos meus projetos em andamento com as stacks utilizadas e que tenho conhecimento, espero que goste :)'
             currBulma.innerHTML='Currículo com Bulma'
             liveChat.innerHTML='CoffeBreak Chat em Tempo real'
+            socialMedia.innerHTML='Redes sociais:'
             
         } else {
             const h1 = document.querySelector('header h1')
             const h2 = document.querySelector('header h2')
             const currBulma = document.querySelector('#currBulma')
             const liveChat = document.querySelector('#liveChat')
+            const socialMedia = document.querySelector('.userMedia p')
             h1.innerHTML='Hi, this website creator is Vinicius de Andrade!'
             h2.innerHTML='Here you can find all my projects with the stacks im currently working, hope you enjoy :)'
             currBulma.innerHTML='Curriculum with Bulma'
             liveChat.innerHTML='CoffeBreak RealTime Chat'
-        }
-    }
+            socialMedia.innerHTML='Social Media:'
+    }}
 )
 
-const toggle = document.querySelector('#toggle')
+
+const toggleTheme = document.querySelector('#toggleTheme')
 const body = document.querySelector('body')
-toggle.addEventListener('click',()=>{
+toggleTheme.addEventListener('click',()=>{
     body.classList.toggle('whiteTheme')
-    toggle.classList.toggle('whiteTheme')
+    toggleTheme.classList.toggle('whiteTheme')
 })
