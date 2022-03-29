92d26bb4f02014906c1b422ddcc058bca36674c0
andradeviniicius <viniciusdandrade01@gmail.com>
2022-03-29 16:04:01 -0300

    fixinng height on itemDetails

--- simplePort/scripts/main.js
+++ simplePort/scripts/main.js
@@ -11,14 +11,15 @@ toggleLanguage.addEventListener('click', () => {
         h1.innerHTML = 'Olá, seja bem vindo ao meu portifólio!'
         h2.innerHTML = 'Aqui você consegue acessar todos meus projetos em andamento com as stacks utilizadas e que tenho conhecimento, espero que goste :)'
         socialMedia.innerHTML = 'Redes sociais:'
-        itemDetails.classList.toggle('heightFix')
+        itemDetails.classList.add('heightFix')
         itemDetails.innerText = 'Esse projeto foi criado com o intuito de disponibilizar um ambiente agradável para desenvolvedores terem a possibilidade de um coffeBreak em home-office. Acredito na importancia de um momento de distração durante a jornada de trabalho e no home-office o momento do cafézinho se perdeu, por isso a ideia do projeto, O MVP que seria o chat já está disponibilizado e futuramente outras funções serão implementadas como uma radio colaborativa, mensagens privadas, cadastro de usurio e por aí vai :)';
-
+        
     } else {
         const h1 = document.querySelector('header h1')
         const h2 = document.querySelector('header h2')
         const liveChat = document.querySelector('#liveChat')
         const socialMedia = document.querySelector('.userMedia p')
+        itemDetails.classList.remove('heightFix')
         h1.innerHTML = 'Hi, Welcome to my portifolio!'
         h2.innerHTML = 'Here you can find all my projects with the stacks im currently working, hope you enjoy :)'
         liveChat.innerHTML = 'CoffeBreak RealTime Chat'
