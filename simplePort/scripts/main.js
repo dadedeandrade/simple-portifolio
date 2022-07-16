b0d64a50d7fc75fe94276e02ae300844eddb490b
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-16 19:07:51 -0300

    feature/ first version of pokedex item

--- simplePort/scripts/main.js
+++ simplePort/scripts/main.js
@@ -6,6 +6,7 @@ toggleLanguage.addEventListener('click', () => {
         const h1 = document.querySelector('header h1')
         const h2 = document.querySelector('header h2')
         const liveChat = document.querySelector('#liveChat')
+        const pokedex = document.querySelector('#pokedex')
         const socialMedia = document.querySelector('.userMedia p')
         const itemDetails = document.querySelector('.itemDetails')
         h1.innerHTML = 'Olá, seja bem vindo ao meu portifólio!'
@@ -18,11 +19,13 @@ toggleLanguage.addEventListener('click', () => {
         const h1 = document.querySelector('header h1')
         const h2 = document.querySelector('header h2')
         const liveChat = document.querySelector('#liveChat')
+        const pokedex = document.querySelector('#pokedex')
         const socialMedia = document.querySelector('.userMedia p')
         itemDetails.classList.remove('heightFix')
         h1.innerHTML = 'Hi, Welcome to my portifolio!'
         h2.innerHTML = 'Here you can find all my projects with the stacks im currently working, hope you enjoy :)'
         liveChat.innerHTML = 'CoffeBreak RealTime Chat'
+        pokedex.innerHTML = 'PokedexAPP'
         socialMedia.innerHTML = 'Social Media:'
         itemDetails.innerText = "The idea behind this project was to create a pleasant environment for developers so they can take a small break from home-office in a chat with their colleagues. I believe in the idea that moments of distraction are one of the most important things during the working day so we can keep productivity going and keep burnout away, and with the current ascension of homeoffice we are kinda losing that little break we had in the office, thats why i developed the app. The MVP is ready, and for the future i'll implement more features like a collaborative radio, private messages, user registration and more :)";
     }
@@ -75,19 +78,10 @@ function removeActiveClassOnHover(elem) {
     document.querySelector(`#${elem}`).classList.remove('active')
 }
 
-
-
-const itemDetails = document.querySelector('.livechatItemDetails')
-liveChat.addEventListener('mouseover', () => {
-
-})
-
-
 function bootingScreen() {
 
     setTimeout(() => document.getElementById('bootSection').classList.add('booting-finish'), 5000)
     setTimeout(() => document.getElementById('bootText').classList.add('booting-finish'), 5000)
 
 }
-
 bootingScreen()
