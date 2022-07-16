3729c49ecfe671e5bcae72cd245041bc3518e893
andradeviniicius <viniciusdandrade01@gmail.com>
2022-07-16 20:37:20 -0300

    feature/ pokedexAPP finished styling

--- simplePort/scripts/main.js
+++ simplePort/scripts/main.js
@@ -18,26 +18,29 @@ toggleLanguage.addEventListener('click', () => {
         pokedexItemDetails.classList.add('heightFix')
         foodwebItemDetails.classList.add('heightFix')
         livechatItemDetails.innerText = 'Esse projeto foi criado com o intuito de disponibilizar um ambiente agradável para desenvolvedores terem a possibilidade de um coffeBreak em home-office. Acredito na importancia de um momento de distração durante a jornada de trabalho e no home-office o momento do cafézinho se perdeu, por isso a ideia do projeto, O MVP que seria o chat já está disponibilizado e futuramente outras funções serão implementadas como uma radio colaborativa, mensagens privadas, cadastro de usurio e por aí vai :)';
-        pokedexItemDetails.innerText = 'Esse projeto foi criado com o intuito de disponibilizar um ambiente agradável para desenvolvedores terem a possibilidade de um coffeBreak em home-office. Acredito na importancia de um momento de distração durante a jornada de trabalho e no home-office o momento do cafézinho se perdeu, por isso a ideia do projeto, O MVP que seria o chat já está disponibilizado e futuramente outras funções serão implementadas como uma radio colaborativa, mensagens privadas, cadastro de usurio e por aí vai :)';
-        foodwebItemDetails.innerText = 'Esse projeto foi criado com o intuito de disponibilizar um ambiente agradável para desenvolvedores terem a possibilidade de um coffeBreak em home-office. Acredito na importancia de um momento de distração durante a jornada de trabalho e no home-office o momento do cafézinho se perdeu, por isso a ideia do projeto, O MVP que seria o chat já está disponibilizado e futuramente outras funções serão implementadas como uma radio colaborativa, mensagens privadas, cadastro de usurio e por aí vai :)';
+        pokedexItemDetails.innerText = "Um app de pokedex criado em colaboração com o @KenjiWorkES utilizando o ReduxToolkit para o frontend e consumindo os dados das api's PokemonAPI e NewsAPI";
+        foodwebItemDetails.innerText = "Um blog que contém artigos e receitas de comidas deliciosas de todo o mundo";
         
     } else {
         const h1 = document.querySelector('header h1')
         const h2 = document.querySelector('header h2')
         const liveChat = document.querySelector('#liveChat')
         const pokedex = document.querySelector('#pokedex')
+        const livechatItemDetails = document.querySelector('.livechatItemDetails')
+        const pokedexItemDetails = document.querySelector('.pokedexItemDetails')
+        const foodwebItemDetails = document.querySelector('.foodwebItemDetails')
         const socialMedia = document.querySelector('.userMedia p')
-        livechatItemDetails.classList.remove('heightFix')
-        pokedexItemDetails.classList.remove('heightFix')
-        foodwebItemDetails.classList.remove('heightFix')
         h1.innerHTML = 'Hi, Welcome to my portifolio!'
         h2.innerHTML = 'Here you can find all my projects with the stacks im currently working, hope you enjoy :)'
         liveChat.innerHTML = 'CoffeBreak RealTime Chat'
         pokedex.innerHTML = 'PokedexAPP'
         socialMedia.innerHTML = 'Social Media:'
+        livechatItemDetails.classList.remove('heightFix')
+        pokedexItemDetails.classList.remove('heightFix')
+        foodwebItemDetails.classList.remove('heightFix')
         livechatItemDetails.innerText = "The idea behind this project was to create a pleasant environment for developers so they can take a small break from home-office in a chat with their colleagues. I believe in the idea that moments of distraction are one of the most important things during the working day so we can keep productivity going and keep burnout away, and with the current ascension of homeoffice we are kinda losing that little break we had in the office, thats why i developed the app. The MVP is ready, and for the future i'll implement more features like a collaborative radio, private messages, user registration and more :)";
-        pokedexItemDetails.innerText = "The idea behind this project was to create a pleasant environment for developers so they can take a small break from home-office in a chat with their colleagues. I believe in the idea that moments of distraction are one of the most important things during the working day so we can keep productivity going and keep burnout away, and with the current ascension of homeoffice we are kinda losing that little break we had in the office, thats why i developed the app. The MVP is ready, and for the future i'll implement more features like a collaborative radio, private messages, user registration and more :)";
-        foodwebItemDetails.innerText = "The idea behind this project was to create a pleasant environment for developers so they can take a small break from home-office in a chat with their colleagues. I believe in the idea that moments of distraction are one of the most important things during the working day so we can keep productivity going and keep burnout away, and with the current ascension of homeoffice we are kinda losing that little break we had in the office, thats why i developed the app. The MVP is ready, and for the future i'll implement more features like a collaborative radio, private messages, user registration and more :)";
+        pokedexItemDetails.innerText = "A pokedex App created in collaboration with @KenjiWorkES using ReduxToolkit for frontend and consuming data from PokemonAPI and NewsAPI";
+        foodwebItemDetails.innerText = "A blog website that contains article and recipes of delicious food from all over the world";
     }
 }
 )
