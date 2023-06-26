30a56c3c34653ad10957c8677c0e493be860e499
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-06-26 07:03:40 -0300

    fix/ changed text in coffebreak

--- scripts/main.js
+++ scripts/main.js
@@ -35,7 +35,7 @@ toggleLanguage.addEventListener("click", () => {
     findYourDuoItemDetails.classList.add("heightFix");
     webOsItemDetails.classList.add("heightFix");
     livechatItemDetails.innerText =
-      "Esse projeto foi criado com o intuito de disponibilizar um ambiente agradável para desenvolvedores tere m a possibilidade de um coffeBreak em home-office. Acredito na importancia de um momento de distração durante a jornada de trabalho e no home-office o momento do cafézinho se perdeu, por isso a ideia do projeto, O MVP que seria o chat já está disponibilizado e futuramente outras funções serão implementadas como uma radio colaborativa, mensagens privadas, cadastro de usurio e por aí vai :)";
+      "Esse projeto foi criado com o intuito de disponibilizar um ambiente agradável para desenvolvedores terem a possibilidade de um coffeBreak em home-office. Acredito na importancia de um momento de distração durante a jornada de trabalho e no home-office o momento do cafézinho se perdeu, por isso a ideia do projeto, O MVP que seria o chat já está disponibilizado e futuramente outras funções serão implementadas como uma radio colaborativa, mensagens privadas, cadastro de usurio e por aí vai :)";
     findYourDuoItemDetails.innerText =
       "Melhore a sua experiência de jogo com esse app para encontrar o duo ideal e conectar através do Discord. Filtre por jogo e estilo de jogo assim como dia da semana e horário disponível.";
     webOsItemDetails.innerText =
@@ -62,7 +62,7 @@ toggleLanguage.addEventListener("click", () => {
     socialMedia.innerText = "Social Media:";
     livechatItemDetails.classList.remove("heightFix");
     livechatItemDetails.innerText =
-      "The idea behind this project was to create a pleasant environment for developers so they can take a small break from home-office in a chat with their colleagues. I believe in the idea that moments of distraction are one of the most important things during the working day so we can keep productivity going and keep burnout away, and with the current ascension of homeoffice we are kinda losing that little break we had in the office, thats why i developed the app. The MVP is ready, and for the future i'll implement more features like a collaborative radio, private messages, user registration and more :)";
+      "The project aims to create a pleasant environment for developers to take a small break from home-office and chat with colleagues. The goal is to provide moments of distraction to maintain productivity, prevent burnout, and compensate for the lack of breaks in a home-office setting. The MVP is complete, and future plans include adding features such as a collaborative radio, private messages, user registration, and more. :)";
     webOsItemDetails.innerText =
       "A replication of a operational system (Linux) using HTML, CSS and JS";
     findYourDuoItemDetails.innerText =
